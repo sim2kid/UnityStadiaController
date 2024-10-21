1a3386e0ac8c781adff011e164f025cc5d6bc56f
Owen Ravelo <sim2kid@users.noreply.github.com>
2024-10-21 14:21:23 -0500

    Add namespace to StadiaInput scripts
    Enclosed the StadiaController and StadiaControllerState classes in the new `sim2kid.Package.StadiaInput` namespace. This helps organize the scripts better and reduces potential naming conflicts.

--- Assets/Scripts/Package/StadiaInput/StadiaController.cs
+++ Assets/Scripts/Package/StadiaInput/StadiaController.cs
@@ -1,48 +1,51 @@
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.Layouts;
 using UnityEngine.InputSystem.Controls;
+using UnityEngine.InputSystem.Layouts;
 
-[InputControlLayout(stateType = typeof(StadiaControllerState))]
-#if UNITY_EDITOR
-[InitializeOnLoad] // Make sure static constructor is called during startup.
-#endif
-public class StadiaController : Gamepad
+namespace sim2kid.Package.StadiaInput
 {
-    static StadiaController()
+    [InputControlLayout(stateType = typeof(StadiaControllerState))]
+#if UNITY_EDITOR
+    [InitializeOnLoad] // Make sure static constructor is called during startup.
+#endif
+    public class StadiaController : Gamepad
     {
-        // Match device via Name and Manufacturer
-        InputSystem.RegisterLayout<StadiaController>(
-            "Stadia Controller",
-            new InputDeviceMatcher()
-                .WithInterface("HID")
-                .WithManufacturer("Google Inc.")
-                .WithProduct("Stadia Controller"));
+        static StadiaController()
+        {
+            // Match device via Name and Manufacturer
+            InputSystem.RegisterLayout<StadiaController>(
+                "Stadia Controller",
+                new InputDeviceMatcher()
+                    .WithInterface("HID")
+                    .WithManufacturer("Google Inc.")
+                    .WithProduct("Stadia Controller"));
 
-        // OR match device via VendorID and Product ID
-        InputSystem.RegisterLayout<StadiaController>(
-            "Stadia Controller",
-            new InputDeviceMatcher()
-                .WithInterface("HID")
-                .WithCapability("vendorId", 0x18D1) // Google Inc.
-                .WithCapability("productId", 0x9400)); // Stadia Controller *
-    }
+            // OR match device via VendorID and Product ID
+            InputSystem.RegisterLayout<StadiaController>(
+                "Stadia Controller",
+                new InputDeviceMatcher()
+                    .WithInterface("HID")
+                    .WithCapability("vendorId", 0x18D1) // Google Inc.
+                    .WithCapability("productId", 0x9400)); // Stadia Controller *
+        }
 
-    // In the Player, to trigger the calling of the static constructor,
-    // create an empty method annotated with RuntimeInitializeOnLoadMethod.
-    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-    static void Init()
-    { }
+        // In the Player, to trigger the calling of the static constructor,
+        // create an empty method annotated with RuntimeInitializeOnLoadMethod.
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        static void Init()
+        { }
     
-    public ButtonControl captureButton { get; protected set; }
-    public ButtonControl googleButton { get; protected set; }
+        public ButtonControl captureButton { get; protected set; }
+        public ButtonControl googleButton { get; protected set; }
 
-    protected override void FinishSetup()
-    {
-        captureButton = GetChildControl<ButtonControl>("captureButton");
-        googleButton = GetChildControl<ButtonControl>("googleButton");
+        protected override void FinishSetup()
+        {
+            captureButton = GetChildControl<ButtonControl>("captureButton");
+            googleButton = GetChildControl<ButtonControl>("googleButton");
 
-        base.FinishSetup();
+            base.FinishSetup();
+        }
     }
 }
